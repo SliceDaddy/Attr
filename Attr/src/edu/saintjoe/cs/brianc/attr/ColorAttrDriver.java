263f8cdcd67974fb4cf4b527e24902c2d7ec5bfa
SliceDaddy <rslipher@saintjoe.edu>
2015-04-24 15:52:01 -0400

    ColorAttrDriver

---
+++ Attr/src/edu/saintjoe/cs/brianc/attr/ColorAttrDriver.java
@@ -0,0 +1,12 @@
+package edu.saintjoe.cs.brianc.attr;
+
+public class ColorAttrDriver {
+	
+	public static void main(String[] args) {
+		
+		ColorAttr testColor;
+		
+		testColor = new ColorAttr("red");
+		System.out.println(testColor.toString());
+	}
+}
