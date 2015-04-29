9ee4cc4913c414e0c26953a1aa71f0efc88268ca
SliceDaddy <rslipher@saintjoe.edu>
2015-04-29 15:22:12 -0400

    added code

--- Attr/src/edu/saintjoe/cs/brianc/attr/ColorAttrDriver.java
+++ Attr/src/edu/saintjoe/cs/brianc/attr/ColorAttrDriver.java
@@ -4,9 +4,29 @@ public class ColorAttrDriver {
 	
 	public static void main(String[] args) {
 		
-		ColorAttr testColor;
+		ColorAttr testOne, testTwo, testThree;
+		Attr testFour;
 		
-		testColor = new ColorAttr("red");
-		System.out.println(testColor.toString());
-	}
-}
+		// We need this object for the third ColorAttr constructor
+		ScreenColor newColor = new ScreenColor("green");
+		
+		// Test out our constructors
+		
+		// Number one
+		testOne = new ColorAttr("ColorOne","red");
+		System.out.println(testOne.toString());
+		
+		//Number two
+		testTwo = new ColorAttr("ColorTwo");
+		System.out.println(testTwo.toString());
+		
+		// Number Three
+		testThree = new ColorAttr("ColorThree", newColor);
+		System.out.println(testThree.toString());
+		
+		// Number four
+				testFour = new Attr("ColorFour", "yellow");
+				System.out.println(testFour.toString());
+				
+			} // end main
+}	
